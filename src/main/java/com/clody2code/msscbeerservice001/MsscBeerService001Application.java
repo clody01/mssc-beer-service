42b84169216788735c19d5e377bfb75f917208e5
Clody01 <tseoue01@gmail.com>
2021-08-10 03:16:34 +0200

    initial project add. Closes #1

---
+++ src/main/java/com/clody2code/msscbeerservice001/MsscBeerService001Application.java
@@ -0,0 +1,13 @@
+package com.clody2code.msscbeerservice001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerService001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerService001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/clody2code/msscbeerservice001/MsscBeerService001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clody2code.msscbeerservice001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerService001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
